973e62ba33bdf97f7b4772d96eb93a2933341933
Christopher Harden <Harden.Christopher00@gmail.com>
2022-05-04 01:28:33 -0500

    passing two tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,28 @@
-def read_template()
+def read_template(x):
+    try:
+        file  = open(x)
+        contents = file.read()
+        return contents
+    except FileNotFoundError:
+        raise FileNotFoundError
+        print("file not found")
 
-def parse_template()
+def parse_template(x,y):
+    actual_stripped = ("It was a {} and {} {}."), 
+    actual_parts = filter(lambda x: x.startswith(("{")))
+    print (actual_parts)
+    return actual_stripped, actual_parts
 
-def ()
+
+def merge():
+    pass
+'''
+
+actual_stripped
+
+actual_parts
+
+
+'''
+
+
