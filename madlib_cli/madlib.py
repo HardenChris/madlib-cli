236e5405ff65598208e49a96e506cb5b3868e15a
Christopher Harden <Harden.Christopher00@gmail.com>
2022-05-04 01:41:06 -0500

    passing three tests

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -7,15 +7,15 @@ def read_template(x):
         raise FileNotFoundError
         print("file not found")
 
-def parse_template(x,y):
+def parse_template(actual_stripped, actual_parts):
     actual_stripped = ("It was a {} and {} {}."), 
     actual_parts = filter(lambda x: x.startswith(("{")))
     print (actual_parts)
     return actual_stripped, actual_parts
 
 
-def merge():
-    pass
+def merge(x, y):
+   return x.format(*y)
 '''
 
 actual_stripped
